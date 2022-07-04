2269fb8a742a330f775eb42e7c19ee35d5c1191f
Ylan <99525156+YlanNaly@users.noreply.github.com>
2022-07-04 13:50:58 +0300

    Finalisation of my Server for Chat real time

--- server.js
+++ server.js
@@ -1,29 +1,59 @@
-let http = require('http');
-let fs = require('fs') ;
-let url = require('url');
+const app = require("express")(),
+  colors = require("colors"),
+  httpServer = require("http").createServer(app),
+  io = require("socket.io")(httpServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  }),
+  PORT = 3006;
+var room = null;
+var receiverTyping = null;
+const users = {};
+io.on("connection", (socket) => {
+  console.log(`someone connect and socket id ${socket.id}`.bgBlue.white);
 
-let server = http.createServer()
-let query ;
-let name;
-server.on('request',(request,response) => {
+  // CREATE NEW USER
+  socket.on("new_user", (userData) => {
+    room = userData.room;
+    console.log('user data'.bgYellow.red,userData);
+    console.log("username :".bgYellow.black, userData.username);
+    console.log(`room:${userData.room}`.bgBlue.white);
+    users[userData.username] = socket.id;
+    io.emit("new_user", users);
+  });
 
-    fs.readFile('./client/index.html' , 'utf-8' , (err,data) =>{
-        if(err) {
-            response.writeHead(404);
-            response.end('fichier introuvable')
-        }
-        
-        else{
-        response.writeHead(200, {
-            'Content-type' : 'text/html ; charset = utf-8'
-        });
+  // SELECT USER TO CONNECT WITH AND JOIN ROOM
+  socket.on("user_select", (data) => {
+    console.log("data user_select", data);
+    console.log("sender", data.sender);
+    console.log("receiver", data.receiver);
+    receiverTyping = data.receiver
+    socket.join(room);
+  });
 
-         query = url.parse(request.url , true).query
-         name = query.name === undefined ? 'ok' : query.name;
-    }
-    data = data.replace('{{name}}' , name)
-    response.end(data)
-    })
-})
+  // SEND MESSAGE INTO A SPECIFIC ROOM
+  socket.on("send_message", (messageData) => {
+    console.log("messageData in need".bgCyan.white, messageData);
+    socket.to(room).emit("new_message", messageData);
+  });
 
-server.listen(8080)
+  // CHECK IF SOMEONE IS TYPING
+  socket.on("someone_isTyping", (isTyping) => {
+    // console.log(`someone is typing :${isTyping}`.bgCyan.white);
+    console.log('users'.bgCyan.red,users);
+    io.to(receiverTyping).emit('check_typing', isTyping)
+
+  });
+
+  // CHECK USER DISCONNECTED
+  socket.on("disconnect", () => {
+    console.log(`user with id: ${socket.id} has disconnected `);
+    console.log("users".bgYellow.blue, users);
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log("server running on port", PORT);
+});
